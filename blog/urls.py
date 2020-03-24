b8ed9700f078eee8a7673d07027a89586631de12
Marcus Thompson <marcus.thompson5@yahoo.com>
2020-03-24 10:56:20 -0700

    Add detail view for each post

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     #if you go to local host it will take you here
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
