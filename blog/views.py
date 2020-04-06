c47179733591da0ccb1987084f5950e016a8f99d
Marcus Thompson <marcus.thompson5@yahoo.com>
2020-04-06 12:02:12 -0700

    Add feature to add comment on blog post

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm, CommentForm
 
-from .forms import PostForm
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -56,8 +57,23 @@ def post_draft_list(request):
     stuff_for_frontend = {'posts':posts}
     return render(request, 'blog/post_draft_list.html', stuff_for_frontend)
 
+@login_required
 def post_publish(reqeust, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'Post':
+        form = CommentForm(reqeust.Post)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form':form})
