50a4a573bd9f55c6eb4ed5b07b1d93652ffe8c1c
Marcus Thompson <marcus.thompson5@yahoo.com>
2020-04-06 14:32:33 -0700

    Add feature to approve comments

--- blog/views.py
+++ blog/views.py
@@ -64,9 +64,9 @@ def post_publish(reqeust, pk):
     return redirect('post_detail', pk=pk)
 
 def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post,pk=pk)
-    if request.method == 'Post':
-        form = CommentForm(reqeust.Post)
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.author = request.user
@@ -82,3 +82,8 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail',pk=comment.post.pk)
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk )
